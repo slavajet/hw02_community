bd10ded289b0ca53d766b70f6ad02083ec47763a
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-02-27 19:30:00 +0600

    Подключил контекст процессор year

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -45,10 +45,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.year.year',
             ],
         },
     },
-]
+] 
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
