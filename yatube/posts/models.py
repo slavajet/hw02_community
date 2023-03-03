1fd344272a3f48465d614458836272740af1c4b3
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-03-03 21:15:40 +0600

    настроил profile и posr_detail все работает

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
     def __str__(self):
         return self.text
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(unique=True)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,20 +2,21 @@ from django.core.paginator import Paginator
 
 from django.shortcuts import get_object_or_404, render
 
-from .models import Group, Post
+from .models import Group, Post, User
+
 
 POSTS_PER_PAGE = 10
 
 
 def index(request):
     post_list = Post.objects.all().order_by('-pub_date')
-    paginator = Paginator(post_list, POSTS_PER_PAGE) 
+    paginator = Paginator(post_list, POSTS_PER_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
@@ -26,3 +27,25 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=author)
+    paginator = Paginator(posts, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+        'posts': posts,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    full_post = get_object_or_404(Post, id=post_id)
+    context = {
+        'full_post': full_post,
+    }
+    return render(request, 'posts/post_detail.html', context)
